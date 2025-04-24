2a19ea64ef3d2fa1405ea47c4ce1162c1d77ff42
894risper <zilper894@gmail.com>
2025-04-24 16:15:04 +0300

    first submit

---
+++ intro/lists.py
@@ -0,0 +1,20 @@
+fruits=["apple","orange","banana"]
+print(fruits)
+print(fruits[0])
+
+if "mango" in fruits:
+    print("yes mango is in the list")
+    
+else:
+    print("mango is not in the list")
+    
+    
+fruits[1]="guava"
+print(fruits);
+
+
+fruits.append("pears")
+print(fruits);
+
+fruits.remove("guava")
+print(fruits)
