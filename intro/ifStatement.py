47756b510dc8ad36c630e9ace85eae02abd3bd96
894risper <zilper894@gmail.com>
2025-04-24 17:08:32 +0300

    the if and while loops

---
+++ intro/ifStatement.py
@@ -0,0 +1,32 @@
+a=200
+b=20
+
+if a > b :
+    print("a is  greater than b")
+    
+elif a==b:
+    print(" a and b are equal")
+    
+else:
+    print('a is less than b')
+    
+    
+    
+# and - the and keyword is a logical operator , and its used to combine conditional statements
+
+
+x=200
+y=33
+c=500
+
+if a>b and c>a:
+    print("both conditions are true")
+
+
+
+age = input ("enter your age")
+
+if  int(age) < 18:
+    print("you ae  minor you cant vote")
+    
+else: print("you are elligible to vote")
---
+++ intro/whileLoop.py
@@ -0,0 +1,14 @@
+#the while loop can execute a set of statement s lopng as a condition is true
+i=1
+while i < 6:
+    print(i)
+    i+=1 
+    
+#break statement - with the break statement we can stop the loop even if the while statement is true
+print("using break")
+j= 1
+while j < 10:
+    print(j)
+    if j== 5:
+        break
+    j +=1
