644966f02dae031af09cebc5eac927ad52eb755f
894risper <zilper894@gmail.com>
2025-06-12 23:50:04 +0300

    functions

---
+++ intro/functions.py
@@ -0,0 +1,13 @@
+# a function is block of code which only runs when it is called
+
+def my_function():
+    print('hello word')
+    
+my_function()
+ 
+ #information can be passed into functions as arguments
+ 
+def my_name(fname):
+    print(fname  +"angel ")  
+    
+my_name("Risper ") 
